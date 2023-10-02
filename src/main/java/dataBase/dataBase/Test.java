b0e703aeac13f5a14f495d7ac604dc84d581ff82
agusdigiam <agustindigiambatista@gmail.com>
2023-10-02 18:01:02 -0300

    DataBase

---
+++ src/main/java/dataBase/dataBase/Test.java
@@ -0,0 +1,17 @@
+package dataBase.dataBase;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class Test {
+    @GetMapping("/test")
+    List<String> Test(){
+        return List.of("Funcionando" , "Perfectamente");
+    }
+}
