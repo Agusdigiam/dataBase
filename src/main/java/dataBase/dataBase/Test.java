e206c4d16eea0a03805d7cd616c7db68fee32dd4
agusdigiam <agustindigiambatista@gmail.com>
2023-10-02 18:05:03 -0300

    DataBase

--- src/main/java/dataBase/dataBase/Test.java
+++ src/main/java/dataBase/dataBase/Test.java
@@ -2,7 +2,6 @@ package dataBase.dataBase;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
