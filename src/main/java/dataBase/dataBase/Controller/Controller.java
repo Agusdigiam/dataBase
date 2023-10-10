df5ceb98edf390862316745714b3892971050891
Agusdigiam <agustindigiambatista@gmail.com>
2023-10-10 18:13:54 -0300

    dataBase Spanish Version

--- src/main/java/dataBase/dataBase/Controller/Controller.java
+++ src/main/java/dataBase/dataBase/Controller/Controller.java
@@ -13,19 +13,26 @@ public class Controller {
     @Autowired
     private Repository repository;
 
+    //Para Obtener los empleados usamos el metodo Get
+
     @GetMapping("/empleados")
     public List<Empleado> getEmpleados(){
         return repository.findAll();
     }
+
+
+    //Para Guardar un Usuario, usamos el metodo Post
     @PostMapping(value ="/guardarempleados")
     public String guardarEmpleados(@RequestBody Empleado empleado){
         repository.save(empleado);
-        return "Empleado Guardado en la Base de Datos.";
+        return "Empleado Guardado Correctamente en la Base de Datos.";
 
     }
+
+    //Para Borrar un Usuario, usamos el metodo Delete
     @DeleteMapping(value = "/borrarempleados")
     public String borrarEmpleado(@RequestBody Empleado empleado){
         repository.delete(empleado);
-        return "Empleado Borrado Correctamente";
+        return "Empleado Borrado Correctamente de la Base de Datos";
     }
 }
